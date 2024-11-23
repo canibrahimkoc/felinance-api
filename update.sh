9531e24ad5c73f73e9e5b8215ac740fe2e9dde42
root <root@CK>
2024-11-23 12:17:35 +0300

    v0.2

--- update.sh
+++ update.sh
@@ -1,45 +1,47 @@
+#!/bin/bash
 
-git_repo=git config --get remote.origin.url
-git_url=pwd
+git_repo=$(git config --get remote.origin.url)  # Git reposunun URL'sini al
+git_url=$(pwd)  # Geçerli dizinin yolunu al
 
-cd "$git_url" || return 1
+cd "$git_url" || exit 1  # Git repo dizinine git, hata durumunda çık
 
 if [ ! -d ".git" ]; then
-    git init
+    git init  # Eğer Git repo'su yoksa başlat
 fi
-git remote set-url origin "$git_repo"
+
+git remote set-url origin "$git_repo"  # Uzak repo URL'sini ayarla
 
 if ! git show-ref --verify --quiet refs/heads/main; then
     echo "Main branch does not exist. Creating it now..."
-    git checkout -b main
+    git checkout -b main  # Main branch yoksa oluştur
 fi
 
-git config pull.rebase false && git fetch origin
-commit_count=$(git rev-list --count HEAD)
+git config pull.rebase false && git fetch origin  # Uzak repo'dan verileri çek
+commit_count=$(git rev-list --count HEAD)  # Toplam commit sayısını al
 
 if [ $commit_count -eq 0 ]; then
-    version="v0.1"
+    version="v0.1"  # İlk commitse v0.1
 else
     major_version=$((commit_count / 10))
     minor_version=$((commit_count % 10))
-    version="v${major_version}.${minor_version}"
+    version="v${major_version}.${minor_version}"  # Sürüm numarasını oluştur
 fi
 
-LOCAL=$(git rev-parse HEAD)
-REMOTE=$(git rev-parse origin/main)
+LOCAL=$(git rev-parse HEAD)  # Lokal commit ID'sini al
+REMOTE=$(git rev-parse origin/main)  # Uzak main branch commit ID'sini al
 
 if [ "$LOCAL" != "$REMOTE" ]; then
     echo "New version available, pulling updates..."
-    git pull origin main --no-edit
+    git pull origin main --no-edit  # Eğer farklı ise, güncellemeleri çek
 else
     echo "You are on the latest version. $git_url $version"
 fi
 
 if [[ $(git status --porcelain) ]]; then
     echo "Local changes detected, committing..."
-    git add .
-    git commit -m "$version"
-    git push origin main
+    git add .  # Yerel değişiklikleri ekle
+    git commit -m "$version"  # Commit mesajı olarak sürüm numarasını kullan
+    git push origin main  # Değişiklikleri uzak repo'ya gönder
     echo "Changes successfully pushed. New version: $git_url $version"
 else
     echo "No local changes. Current version: $git_url $version"
